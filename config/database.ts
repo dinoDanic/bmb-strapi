1a0086216984729b589392c39041385f217af50d
dinoDanic <dino.danic@gmail.com>
2023-01-12 21:57:16 +0100

    db

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5431),
-      database: env("DATABASE_NAME", "dino"),
-      user: env("DATABASE_USERNAME", "dino"),
-      password: env("DATABASE_PASSWORD", ""),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      host: env("DATABASE_HOST", "135.181.214.24"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "brunobmb_develop"),
+      user: env("DATABASE_USERNAME", "brunobmb_dino"),
+      password: env("DATABASE_PASSWORD", "Ruda,actv1"),
+      // schema: env("DATABASE_SCHEMA", "public"), // Not required
     },
     debug: false,
   },
