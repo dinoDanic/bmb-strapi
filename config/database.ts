1b13edd5d59b4de21b3eea96a61465c504b1a129
dinoDanic <dino.danic@gmail.com>
2023-01-13 22:17:46 +0100

    config dtabase

--- config/database.ts
+++ config/database.ts
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "135.181.214.24"),
+      host: env("DATABASE_HOST", "167.235.150.40"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "brunobmb_develop"),
-      user: env("DATABASE_USERNAME", "brunobmb_dino"),
+      database: env("DATABASE_NAME", "develop"),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "Ruda,actv1"),
       // schema: env("DATABASE_SCHEMA", "public"), // Not required
     },
